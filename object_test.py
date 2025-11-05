da6fede0797e7df0d16ee8a0dc01785098a12788
AdamH-Python <adam.g.hellinga@gmail.com>
2025-11-05 12:28:35 -0500

    Finished Problem 8

--- object_test.py
+++ object_test.py
@@ -10,10 +10,10 @@ class Time:
         self.minutes = minutes
         self.seconds = seconds
         
-def convert_to_seconds(t):
-    minutes = t.hours * 60 + t.minutes
-    seconds = minutes * 60 + t.seconds
-    print(seconds)
+    def convert_to_seconds(self):
+        minutes = self.hours * 60 + self.minutes
+        seconds = minutes * 60 + self.seconds
+        print(seconds)
 
 if __name__ == '__main__':
     import doctest
