4dee1b580af9d8661c2487c89f802ece978ebefb
AdamH-Python <adam.g.hellinga@gmail.com>
2025-11-05 12:26:51 -0500

    Set up doctest and added initial functions

---
+++ object_test.py
@@ -0,0 +1,20 @@
+"""
+    >>> test = Time(2, 15, 30)
+    >>> test.convert_to_seconds()
+    8130
+"""
+# Place your solution code on the line after this one...
+class Time:
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+        
+    def convert_to_seconds(t):
+        minutes = t.hours * 60 + t.minutes
+        seconds = minutes * 60 + t.seconds
+        print(seconds)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
